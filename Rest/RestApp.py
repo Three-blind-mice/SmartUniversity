5f25c6833869ce1d8b2b5728d803b5d59d542fc4
Акимов Владислав <55551128+Candarush@users.noreply.github.com>
2020-09-22 20:44:59 +0300

    Add files via upload

--- Rest/RestApp.py
+++ Rest/RestApp.py
@@ -1,14 +1,24 @@
-"""Запуск Rest"""
 from flask import Flask, json, jsonify
 
 app = Flask(__name__)
 
 with open('users_list.json') as f:
-    users_list = json.load(f)
+	users_list = json.load(f)
 
-@app.route('/users_list', methods=['GET'])
+def check(id) -> str: # Проверка на наличие в списке
+	id = str(id)
+	for user in users_list:
+		if user["id"] == id:
+			return 'True'
+	return 'False'
+
+@app.route('/users_list/', methods=['GET'])
 def get_list():
 	return jsonify(users_list)
+
+@app.route('/users_list/<id>', methods=['GET'])
+def get_user(id):
+	return check(id)
 
 if __name__ == '__main__':
 	app.run()
