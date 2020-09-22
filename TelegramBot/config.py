7b2aae0dcb334245eb1d43da75030cd926fa2efe
DimaUsenko <56971020+DimaUsenko@users.noreply.github.com>
2020-09-22 19:54:12 +0300

    Add files via upload

---
+++ TelegramBot/config.py
@@ -0,0 +1,9 @@
+TOKEN = '1314559230:AAE9bLqadcLXVzAzJzhj_ncnGZ4peE8hxhs' # bot token from @BotFather
+
+USER = 'iunddsnu'
+PASSWORD = '7FpJGsEHSxQA'
+
+SERVER = 'hairdresser.cloudmqtt.com'
+PORT = 15636
+
+admin_id = 967248282
