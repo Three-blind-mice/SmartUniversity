397547644a1a2e42326e9338657c1fef11268ffd
Акимов Владислав <55551128+Candarush@users.noreply.github.com>
2020-09-22 20:59:38 +0300

    new restapp

---
+++ Rest/restapp.py
@@ -0,0 +1,25 @@
+from flask import Flask, json, jsonify
+from settings import *
+
+app = Flask(__name__)
+
+with open('users_list.json') as f:
+	users_list = json.load(f)
+
+def check(id) -> str: # Проверка на наличие в списке
+	id = str(id)
+	for user in users_list:
+		if user["id"] == id:
+			return 'True'
+	return 'False'
+
+@app.route('/users_list/', methods=['GET'])
+def get_list():
+	return jsonify(users_list)
+
+@app.route('/users_list/<id>', methods=['GET'])
+def get_user(id):
+	return check(id)
+
+if __name__ == '__main__':
+	app.run(host = REST_HOST, port = REST_PORT)
