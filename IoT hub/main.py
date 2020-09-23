ebcc64add2603c82aa0f182b4489648792575461
MarkHomeless <mark_homeless@mail.ru>
2020-09-23 20:17:23 +0300

    Revert "Test"
    This reverts commit 614ccb35f59aece4d0b093ec81549512aab26b97.
    
    # Conflicts:
    #	SmartUniversity/IoT hub/exception.py
    #	SmartUniversity/IoT hub/main.py
    #	SmartUniversity/IoT hub/message_handler.py
    #	SmartUniversity/IoT hub/settings.py

--- IoT hub/main.py
+++ IoT hub/main.py
@@ -1,9 +1,8 @@
 from message_handler import MessageHandler
 import time
-from lmsdriver import LmsDriver
+
 
 if __name__ == '__main__':
-
      message_handler = MessageHandler('111')
      while True:
          message_handler.get_message()
