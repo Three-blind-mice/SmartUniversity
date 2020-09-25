bda0c8869265dd4fb7b3078d6f5875fed0212e3b
DimaUsenko <56971020+DimaUsenko@users.noreply.github.com>
2020-09-25 12:19:10 +0300

    Update config.py

--- docker_bot/config.py
+++ docker_bot/config.py
@@ -1,12 +1,12 @@
-TOKEN = '1314559230:AAE9bLqadcLXVzAzJzhj_ncnGZ4peE8hxhs' # bot token from @BotFather
+TOKEN = '...' # bot token from @BotFather
 
-USER = 'iunddsnu'
-PASSWORD = '7FpJGsEHSxQA'
+USER = '...'
+PASSWORD = '...'
 
-SERVER = 'hairdresser.cloudmqtt.com'
-PORT = 15636
+SERVER = '...'
+PORT = ...
 
-admin_id = 967248282
+admin_id = ...
 
 REST_SERVER = '127.0.0.1'#localhost
 REST_PORT = '5000'#5000
