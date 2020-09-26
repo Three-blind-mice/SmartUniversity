6f9138bfac00b7dbf01534bcd1db671edb24d78c
MarkHomeless <mark_homeless@mail.ru>
2020-09-26 11:00:40 +0300

    Commit 26.09.2020

---
+++ TelegramBot/config.py
@@ -0,0 +1,12 @@
+TOKEN = '...' # bot token from @BotFather
+
+USER = '...' #логин mqtt
+PASSWORD = '...'#пароль mqtt
+
+SERVER = '...'#сервер mqtt
+PORT = ...
+
+REST_SERVER = '...'#localhost 
+REST_PORT = '...'#5000
+
+
