7d2d6163f32498544e75b25c1408c954b2ac3eee
DimaUsenko <56971020+DimaUsenko@users.noreply.github.com>
2020-09-26 16:49:08 +0300

    Update MaiSmartUniversity_v1.py

--- TelegramBot/MaiSmartUniversity_v1.py
+++ TelegramBot/MaiSmartUniversity_v1.py
@@ -1,11 +1,9 @@
 import paho.mqtt.client as mqtt
 import config
 import telebot
-import json
 import requests
 import json
 from telebot import types
-
 
 bot = telebot.TeleBot(config.TOKEN)
 user_dict = {}
@@ -14,6 +12,7 @@ client = mqtt.Client()
 
 class ZoomMessage:
     """Класс для хранения данных при запуске трансляции на плафторе ZOOM"""
+
     def __init__(self, name):
         self.platform = name
         self.aud = None
@@ -23,6 +22,7 @@ class ZoomMessage:
 
 class LmsMessage:
     """Класс для хранения данных при запуске трансляции на плафторе LMS.MAI"""
+
     def __init__(self, name):
         self.platform = name
         self.aud = None
@@ -31,12 +31,13 @@ class LmsMessage:
 
 def on_connect(clinet, userdata, flags, rc):
     print("Connected with Code :" + str(rc))
-    client.subscribe("smart_university/response/#") #Бот подписывается на топик для обратной свзяи
+    client.subscribe("smart_university/response/#")  # Бот подписывается на топик для обратной свзяи
 
 
 def on_message(client, userdata, msg):
     callback = json.loads(msg.payload.decode("utf-8", "ignore"))
-    bot.send_message(int(callback['id']), str(callback['answer']))#Получает из обратной связи id и сообщение, посылает его пользователю
+    bot.send_message(int(callback['id']),
+                     str(callback['answer']))  # Получает из обратной связи id и сообщение, посылает его пользователю
 
 
 client.on_connect = on_connect
@@ -62,7 +63,9 @@ def keyboard_2():
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
     btn1 = types.KeyboardButton("ZOOM")
     btn2 = types.KeyboardButton("LMS.MAI")
+    btn3 = types.KeyboardButton("Выйти")
     markup.add(btn1, btn2)
+    markup.add(btn3)
     return markup
 
 
@@ -104,8 +107,10 @@ def send_welcome(message):
     """Бот проверяет наличие пользователя в базе преподавателей, если он там есть, то
     предлагает пользователю выбрать платоформу"""
     if (message.text == '/send') or (message.text == 'Запустить новую трансляцию'):
-        url = "http://"+str(config.REST_SERVER)+":"+str(config.REST_PORT)+"/users_list/" + str(message.chat.id)
-        statement = requests.get(url).text
+        # ЧТОБЫ ЗАРАБОТАЛО С РЕСТОМ - расскоментить следующие 2 строчки, заккоментить 3-ю 
+        # url ="http://"+config.REST_SERVER+":"+config.REST_PORT+"/users_list/"+str(message.chat.id) 
+        # statement = requests.get(url).text
+        statement = "True"
 
         if statement == "True":
             msg = bot.send_message(message.chat.id, 'Какую платформу выберете?', reply_markup=keyboard_2())
@@ -118,25 +123,40 @@ def send_welcome(message):
 
 def check_platform_step(message):
     """В зависимости от выбора пользователя начинает определенную последователньость действий"""
+
     try:
-        if message.text == 'ZOOM':
-            chat_id = message.chat.id
-            platform = message.text
-            send = ZoomMessage(platform)
-            user_dict[chat_id] = send
-            msg = bot.send_message(message.chat.id, 'Введите номер аудитории: ', reply_markup=keyboard_3())
-            bot.register_next_step_handler(msg, zoom_process_aud_step)#посылает на следующий шаг последовательности ZOOM
-        elif message.text == 'LMS.MAI':
-            chat_id = message.chat.id
-            platform = message.text
-            send = LmsMessage(platform)
-            user_dict[chat_id] = send
-            msg = bot.send_message(message.chat.id, 'Введите номер аудитории: ', reply_markup=keyboard_3())
-            bot.register_next_step_handler(msg, lms_process_aud_step)#посылает на следующий шаг последовательности LMS.MAI
+        if message.text != 'Выйти':
+            if message.text == 'ZOOM':
+                chat_id = message.chat.id
+                platform = message.text
+                send = ZoomMessage(platform)
+                user_dict[chat_id] = send
+                msg = bot.send_message(message.chat.id, 'Введите номер аудитории: ', reply_markup=keyboard_3())
+                bot.register_next_step_handler(msg,
+                                               zoom_process_aud_step)  # посылает на следующий шаг последовательности ZOOM
+            elif message.text == 'LMS.MAI':
+                chat_id = message.chat.id
+                platform = message.text
+                send = LmsMessage(platform)
+                user_dict[chat_id] = send
+                msg = bot.send_message(message.chat.id, 'Введите номер аудитории: ', reply_markup=keyboard_3())
+                bot.register_next_step_handler(msg,
+                                               lms_process_aud_step)  # посылает на следующий шаг последовательности LMS.MAI
+            else:
+                msg = bot.send_message(message.chat.id, 'Пожалуйста, воспользуйтесь кнопками внизу диалога!',
+                                       reply_markup=keyboard_2())
+                bot.register_next_step_handler(msg, check_platform_step)
+        else:
+            sti = open('bot_menu_sticker.tgs', 'rb')
+            bot.send_message(message.chat.id, 'Чем займемся?')
+            bot.send_sticker(message.chat.id, sti, reply_markup=keyboard())
     except Exception as e:
         bot.reply_to(message, 'oooops')
 
+
 """Начало последовательности для платформы ZOOM"""
+
+
 def zoom_process_aud_step(message):
     try:
         if message.text != 'Выйти':
@@ -204,7 +224,7 @@ def zoom_process_end_step(message):
 
             topik = 'smart_university/' + str(send.aud) + '/execute_comand'
             client.publish(topik, mqtt_message)
-            bot.send_message(chat_id, 'Трансляция остановлена!', reply_markup=keyboard())
+            bot.send_message(chat_id, 'Запрос отправлен!', reply_markup=keyboard())
         else:
             pass
             sti = open('bot_menu_sticker.tgs', 'rb')
@@ -212,9 +232,13 @@ def zoom_process_end_step(message):
             bot.send_sticker(message.chat.id, sti, reply_markup=keyboard())
     except Exception as e:
         bot.reply_to(message, 'oooops')
+
+
 """Конец последовательности для платформы ZOOM"""
 
 """Начало последовательности для платформы LMS.MAI"""
+
+
 def lms_process_aud_step(message):
     try:
         if message.text != 'Выйти':
@@ -265,7 +289,7 @@ def lms_process_end_step(message):
 
             topik = 'smart_university/' + str(send.aud) + '/execute_comand'
             client.publish(topik, mqtt_message)
-            bot.send_message(chat_id, 'Трансляция остановлена!', reply_markup=keyboard())
+            bot.send_message(chat_id, 'Запрос отправлен!', reply_markup=keyboard())
         else:
             pass
             sti = open('bot_menu_sticker.tgs', 'rb')
@@ -273,6 +297,8 @@ def lms_process_end_step(message):
             bot.send_sticker(message.chat.id, sti, reply_markup=keyboard())
     except Exception as e:
         bot.reply_to(message, 'oooops')
+
+
 """Конец последовательности для платформы LMS.MAI"""
 
 bot.enable_save_next_step_handlers(delay=1)
