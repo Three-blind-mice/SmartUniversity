e49c058754d449ce40802e0a7c88147f436ecfb0
Акимов Владислав <55551128+Candarush@users.noreply.github.com>
2020-10-05 12:06:40 +0300

    Moved properties to settings.py and added exception handling.

--- Rest/restapp.py
+++ Rest/restapp.py
@@ -4,21 +4,25 @@ from flask_mysqldb import MySQL
 from settings import *
 
 app = Flask(__name__)
-app.config['MYSQL_USER'] = 'user'
-app.config['MYSQL_PASSWORD'] = 'pass'
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_DB'] = 'smartuniversity_users'
+app.config['MYSQL_USER'] = MYSQL_USER
+app.config['MYSQL_PASSWORD'] = MYSQL_PASSWORD
+app.config['MYSQL_HOST'] = MYSQL_HOST
+app.config['MYSQL_DB'] = MYSQL_DB
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
 
 @app.route('/users_list/<id>', methods=['GET'])
 def get_user(id):
 	cursor = mysql.connection.cursor()
-	cursor.execute('''SELECT name FROM users WHERE telegram_id = %d''' % int(id))
-	result = cursor.fetchone()
-	if result:
-		return 'True\n{}'.format(result['name'])
-	else:
+	try:
+		cursor.execute('''SELECT %s FROM %s WHERE %s = %d''' % (MYSQL_USERNAME_COLUMN_NAME, MYSQL_TALBE_NAME, MYSQL_TELEGRAM_ID_COLUMN_NAME, int(id)))
+		cursor.execute('''SELECT name FROM users WHERE telegram_id = %d''' % int(id))
+		result = cursor.fetchone()
+		if result:
+			return 'True\n{}'.format(result['name'])
+		else:
+			return 'False'
+	except:
 		return 'False'
 
 if __name__ == '__main__':
